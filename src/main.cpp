8244d96871745c3ed1feb7f740fc8cfee7fd68a8
birland <153282481+birland@users.noreply.github.com>
2025-08-24 15:13:29 +0300

    Added EXIT_FAILURE error return in case of a caught exception that could not be handled in main().

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,11 @@ int main() {
         eng.run();
     } catch (std::exception const& e) {
         fmt::println(stderr, "{}", e.what());
-    } catch (...) { fmt::println(stderr, "Unknown exception"); }
+        return EXIT_FAILURE;
+    } catch (...) {
+        fmt::println(stderr, "Unknown exception");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
